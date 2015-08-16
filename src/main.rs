c2aa9982391e8b2b0daa8d742edcd5b9c49d4da6
Kriegslustig <github@ls7.ch>
2015-08-16 21:54:57 +0200

    Nicen up the output

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,26 @@ use std::net::TcpStream;
 use std::thread;
 
 fn main() {
-  let mut host = "127.0.0.1";
+  let host = "127.0.0.1";
   let from_port : u16 = 1;
   let to_port : u16 = 1000;
   println!("Scanning {} to {} on {}", from_port, to_port, host);
-  let result = (from_port..to_port).map(|port| {
-    thread::spawn(move || {
-      (port, scan_port(host, port))
+  println!("Open tcp ports on {}", host);
+  let result = (from_port..to_port)
+    .map(|port| {
+      thread::spawn(move || {
+        (port, scan_port(host, port))
+      })
     })
-  }).map(|handle| {
-    handle.join().unwrap()
-  }).collect::<Vec<_>>();
-
-  println!("{:?}", result);
+    .map(|handle| {
+      handle.join().unwrap()
+    })
+    .collect::<Vec<_>>()
+    .into_iter()
+    .filter(|scan| scan.1);
+  for scan in result {
+    println!("{}", scan.0);
+  }
 }
 
 fn scan_port(host: &str, port: u16) -> bool {
